9710032e5f307d4da621a8b138a1d8036590f637
BenjaminAston <aston.benjamin@gmail.com>
2025-05-28 18:09:18 +0200

    Påbörjat Startsidan av appen

---
+++ receptforyou-react/src/pages/Home.js
@@ -0,0 +1,9 @@
+import InputField from '../components/Input';
+import { useRecipes } from '../app/useRecipes';
+
+export default function Home() {
+  const { recipes, fetchRecipes, loading } = useRecipes();
+
+
+  """fortsätt"""
+}
