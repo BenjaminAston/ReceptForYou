1da12b0c669e715669cc451f5031d3472171a100
BenjaminAston <aston.benjamin@gmail.com>
2025-05-30 12:58:04 +0200

    Gör om API
    API anropades flertals gånger varje ändring vilket gjorde hemisdan långsam. Nu ändras det först när man tryckt sök recept. SKa även ändra Home

--- receptforyou-react/src/app/useRecipes.js
+++ receptforyou-react/src/app/useRecipes.js
@@ -1,39 +1,40 @@
 // Importerar React hooks
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 // Skapar en hook som hämtar recept
-const useRecipes = (ingredients) => {
-    // Sparar recept och laddningsstatus
+export const useRecipes = () => {
+    
     const [recipes, setRecipes] = useState([]); 
     const [loading, setLoading] = useState(false);
   
-    // Kör när ingredients ändras
-    useEffect (() => {
-     // Om inga ingredienser, gör inget
-      if (!ingredients) return; 
 
-      // Börjar hämta data
-      setLoading(true); 
+    const fetchRecipes = async (ingredients, filters) => {
+      if (!ingredients || ingredients.length === 0) return; 
 
-      // Anropar Spoonacular API
-      axios.get("https://api.spoonacular.com/recipes/findByIngredients", {
-        params: {
-            ingredients: ingredients,
-            number: 5, 
-            apiKey: "893747d593ca4d56ada26778bcbf9b48"
-        }
-    })
-     // När vi får data, spara i recipes
-    .then((res) =>  setRecipes(res.data))
-    // Om det blir fel, skriv ut i konsolen
-    .catch((err) => console.error("Fel vid hämtning:", err))
-    // När det är klart, sluta ladda
-    .finally(() => setLoading (false));
- }, [ingredients]);
+     
+      setLoading(true);
+      try {
+        const { diet, intolerances } = filters;
+        const res = await axios.get('https://api.spoonacular.com/recipes/complexSearch', {
+          params: {
+            includeIngredients: ingredients.join(','),
+            diet,
+            intolerances,
+            number: 5,
+            addRecipeInformation: true,
+            apiKey: '893747d593ca4d56ada26778bcbf9b48',
+          }
+        });
+    
+        setRecipes(res.data.results);
+      } catch (err) {
+        console.error('Fel vid hämtning:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
   
- return {recipes, loading};
-};
-
-export default useRecipes; 
+    return { recipes, fetchRecipes, loading };
+  };
 
