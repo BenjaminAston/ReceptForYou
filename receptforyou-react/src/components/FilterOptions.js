a6ea1ae7be3233e88931b45f1f2760d90541fd0e
BenjaminAston <aston.benjamin@gmail.com>
2025-05-30 12:39:45 +0200

    Fixade så att Kosttyp och Allergier inte är en del av valen

--- receptforyou-react/src/components/FilterOptions.js
+++ receptforyou-react/src/components/FilterOptions.js
@@ -8,14 +8,14 @@ const FilterOptions = ({ filters, setFilters }) => {
   return (
     <div>
       <select name="diet" onChange={handleChange} value={filters.diet}>
-        <option value="">Kosttyp</option>
+        <option value="" disabled hidden>Kosttyp</option>
         <option value="vegetarian">Vegetarisk</option>
         <option value="vegan">Vegansk</option>
         <option value="gluten free">Glutenfri</option>
       </select>
 
       <select name="intolerances" onChange={handleChange} value={filters.intolerances}>
-        <option value="">Allergier</option>
+        <option value="" disabled hidden>Allergier</option>
         <option value="dairy">Laktos</option>
         <option value="gluten">Gluten</option>
         <option value="peanut">Nötter</option>
