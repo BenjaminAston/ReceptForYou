dc21556b3b80576093dfaa940af4e8fae69b9923
helinzamar <164181321+helinzamar@users.noreply.github.com>
2025-06-01 23:17:28 +0200

    Update RecipeList.js

--- receptforyou-react/src/components/RecipeList.js
+++ receptforyou-react/src/components/RecipeList.js
@@ -1,5 +1,11 @@
 import React from "react";
+<<<<<<< Updated upstream
 import RecipeCard from "./RecipeCard";
+=======
+import RecipeWithMusic from "./RecipeWithMusic";
+import RecipeCard from "./RecipeCard";
+import RecipeCard from "./RecipeCard";
+>>>>>>> Stashed changes
 import '../styles/global.css';
 
 const RecipeList = ({ recipes, favorites = [], toggleFavorite }) => {
@@ -11,11 +17,32 @@ const RecipeList = ({ recipes, favorites = [], toggleFavorite }) => {
     <div className="grid">
       {recipes.map((recipe) => {
         const isFavorite = favorites.some(
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+          (fav) =>
+            (fav.idMeal && fav.idMeal === recipe.idMeal) ||
+            (fav.id && fav.id === recipe.id)
+        );
+
+        return (
+          <RecipeWithMusic
+
+ Stashed changes
+>>>>>>> Stashed changes
           (fav) => fav.id === (recipe.id || recipe.idMeal)
         );
 
         return (
+<<<<<<< Updated upstream
           <RecipeCard
+=======
+         
+Updated upstream
+Stashed changes
+Stashed changes
+>>>>>>> Stashed changes
             key={recipe.idMeal || recipe.id}
             recipe={recipe}
             isFavorite={isFavorite}
